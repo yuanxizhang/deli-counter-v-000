f14e841a21a6827303ad81e237fdb8a88b7ed663
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-01 20:15:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,8 @@ def line(katz_deli)
     when 0 
       puts "The line is currently empty."
     else
-      puts 
+      puts "The line is currently: "
       katz_deli.each_with_index do |name, index| 
-        puts "#{(index+1)}
+        puts "#{(index+1)}. 
     
 end
