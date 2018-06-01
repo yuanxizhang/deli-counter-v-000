c8c6711c2505afb972e7793c0e40bd856b918214
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-01 20:37:28 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+# deli is empty at the start of the day
+katz_deli = []
+
+def line(name_array)
+  case name_array.length 
+    when 0 
+      puts "The line is currently empty."
+    else
+      m = "The line is currently:"
+      
+      name_array.each_with_index do |name, index| 
+         m += " #{(index+1)}. #{(name)}"
+      end
+      
+      puts m
+    end
+end
+
+def take_a_number(name_array, name)
+  name_array.push(name)
+  puts "Welcome, #{name}. You are number #{name_array.length} in line."
+end
+
+def now_serving(name_array)
+  case name_array.length
+    when 0 
+      puts "There is nobody waiting to be served!"
+    else
+      puts "Currently serving #{name_array[0]}."
+      name_array.shift
+  end
+end
